d4b36663344d8d780fdd01768f17ef57fdf3311c
Daniil Zimin <danya1zimin@gmail.com>
2025-03-05 23:26:38 +0200

    The 'For' cycle wich calculates the degree has added. In addition to the 'While' cycle.

--- js_hw3.js
+++ js_hw3.js
@@ -12,3 +12,14 @@ function pow(x,y) {
 pow(12,2 );
 
 
+function pow1(x,y) {
+    let degree = 1;
+    let yy = y;
+    for (degree; y > 0; y--) {
+        degree *= x
+        console.log(degree)
+    }
+    console.log("When the number " + x + " is degree of " + yy + " the number " + degree + " is obtained.")
+}
+
+pow1(3,3)
